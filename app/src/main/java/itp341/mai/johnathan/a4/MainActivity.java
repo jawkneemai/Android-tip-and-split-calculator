836d3785343d2aef97de1e9c9afe925523e9978c
Johnny <johnathm@usc.edu>
2016-02-18 01:06:09 -0800

    added functionality to update tip amount and bill total amount. updates automatically after an element is changed.

--- app/src/main/java/itp341/mai/johnathan/a4/MainActivity.java
+++ app/src/main/java/itp341/mai/johnathan/a4/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,29 +14,65 @@ import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText editTextBillAmount;
-    TextView textViewTipPercent;
-    SeekBar seekBarTipPercent;
-    TextView textViewTipTotal;
-    TextView textViewBillTotal;
-    Spinner spinnerSplitAmount;
-    TextView textViewPersonAmount;
+    // Widget Variables
+    EditText mEditTextBillAmount;
+    TextView mTextViewTipPercent;
+    SeekBar mSeekBarTipPercent;
+    TextView mTextViewTipTotal;
+    TextView mTextViewBillTotal;
+    Spinner mSpinnerSplitAmount;
+    TextView mTextViewPersonAmount;
+
+    // Instance Variables
+    double currentBillAmount = 0.0;
+    double currentTipAmount = 15.0;
+    final DecimalFormat df = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextBillAmount = (EditText) findViewById(R.id.editTextBillAmount);
-        textViewTipPercent = (TextView) findViewById(R.id.textViewTipPercent);
-        seekBarTipPercent = (SeekBar) findViewById(R.id.seekBarTipPercent);
-        textViewTipTotal = (TextView) findViewById(R.id.textViewTipTotal);
-        textViewBillTotal = (TextView) findViewById(R.id.textViewBillTotal);
-        spinnerSplitAmount = (Spinner) findViewById(R.id.spinnerSplitAmount);
-        textViewPersonAmount = (TextView) findViewById(R.id.textViewPersonAmount);
+        mEditTextBillAmount = (EditText) findViewById(R.id.editTextBillAmount);
+        mTextViewTipPercent = (TextView) findViewById(R.id.textViewTipPercent);
+        mSeekBarTipPercent = (SeekBar) findViewById(R.id.seekBarTipPercent);
+        mTextViewTipTotal = (TextView) findViewById(R.id.textViewTipTotal);
+        mTextViewBillTotal = (TextView) findViewById(R.id.textViewBillTotal);
+        mSpinnerSplitAmount = (Spinner) findViewById(R.id.spinnerSplitAmount);
+        mTextViewPersonAmount = (TextView) findViewById(R.id.textViewPersonAmount);
 
+
+        // Update bill amount after every time the EditText is touched
+        mEditTextBillAmount.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((mEditTextBillAmount.getText()) == null || mEditTextBillAmount.getText().toString().isEmpty()) {
+                    currentBillAmount = 0.0;
+                    updateInfo();
+                } else {
+                    currentBillAmount = Double.parseDouble(mEditTextBillAmount.getText().toString());
+                    updateInfo();
+                }
+                return false;
+            }
+        });
+
+        // Seekbar Listener
+        mSeekBarTipPercent.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                currentTipAmount = (double)progress;
+                mTextViewTipPercent.setText(String.valueOf(progress));
+                updateInfo();
+            }
+
+            @Override public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
 
     }
 
@@ -59,5 +96,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // Function to update all areas that are update-able
+    private void updateInfo() {
+        mTextViewTipTotal.setText(String.valueOf(df.format((currentTipAmount/100)*currentBillAmount)));
+        mTextViewBillTotal.setText(String.valueOf(df.format(((currentTipAmount/100)*currentBillAmount)+currentBillAmount)));
     }
 }
